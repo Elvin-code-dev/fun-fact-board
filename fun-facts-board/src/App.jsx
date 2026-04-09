d87dcb49fbe80d77cb12b79dd87f15dda2b9f204
Elvin Hrytsyuk <183242754+Elvin-code-dev@users.noreply.github.com>
2026-04-09 10:44:55 -0700

    Fun facts done

--- fun-facts-board/src/App.jsx
+++ fun-facts-board/src/App.jsx
@@ -5,7 +5,12 @@ function App() {
 	return (
 		<div>
 			<FunFactCard fact={"I like soccer!"} />
+			<FunFactCard fact={"Volleyball is our favorite sport!"} />
+			<FunFactCard fact={"Basketball is fun to play!"} />
+			<FunFactCard fact={"Tennis is fun to play!"} />
+			<FunFactCard fact={"Ping pong is fun to play and elvin is better at it!"} />
 		</div>
+		
 	);
 }
 
