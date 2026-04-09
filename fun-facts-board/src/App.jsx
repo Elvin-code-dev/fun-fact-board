4dba5de6497aa016b739c98f9bc5f27c4e83395d
Elvin Hrytsyuk <183242754+Elvin-code-dev@users.noreply.github.com>
2026-04-09 11:26:56 -0700

    Updated fun facts

--- fun-facts-board/src/App.jsx
+++ fun-facts-board/src/App.jsx
@@ -8,22 +8,22 @@ function App() {
 				<div className="ahmed-div">
 					<h1>Ahmed</h1>
 					<FunFactCard fact={"I like soccer!"} />
-					<FunFactCard fact={"Volleyball is our favorite sport!"} />
-					<FunFactCard fact={"Basketball is fun to play!"} />
-					<FunFactCard fact={"Tennis is fun to play!"} />
+					<FunFactCard fact={"I like to play the piano"} />
+					<FunFactCard fact={"I'm from Iraq!"} />
+					<FunFactCard fact={"Ronaldo is the goat."} />
 					<FunFactCard
-						fact={"Ping pong is fun to play and elvin is better at it!"}
+						fact={"Ping pong is fun to play and AHMED is better at it!"}
 					/>
 				</div>
 
 				<div className="elvin-div">
 					<h1>Elvin</h1>
-					<FunFactCard fact={"I like soccer!"} />
-					<FunFactCard fact={"Volleyball is our favorite sport!"} />
-					<FunFactCard fact={"Basketball is fun to play!"} />
-					<FunFactCard fact={"Tennis is fun to play!"} />
+					<FunFactCard fact={"Elvin is better in soccer and thats a fact!"} />
+					<FunFactCard fact={"Volleyball just geos better for Elvin and thats a fact!"} />
+					<FunFactCard fact={"Basketball is to easy to play for Elvin and thats a fact!"} />
+					<FunFactCard fact={"Tennis is fun to play and Elvin is just better and thats a fact!"} />
 					<FunFactCard
-						fact={"Ping pong is fun to play and elvin is better at it!"}
+						fact={"Ping pong is fun to play and elvin is better at then Ahmed and thats a fact!"}
 					/>
 				</div>
 			</section>
